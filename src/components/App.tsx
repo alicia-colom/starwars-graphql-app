64471e716277f4a64f7e74759ac1503b41e2004e
alicia-colom <aliciacolom@gmail.com>
2021-02-15 15:14:28 +0100

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from './logo.svg';
+import '../stylesheets/App.scss';
+
+function App() {
+	return <div className="App">Hello there young jedi</div>;
+}
+
+export default App;
